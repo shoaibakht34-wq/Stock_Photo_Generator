334c8a90b2c1c7216e300a8c5177858f3aca6a76
Shoaib Akhtar <shoaibakht34@gmail.com>
2026-04-24 04:20:10 +0530

    Update CORS configuration to allow new origin

--- spring-ai/src/main/java/com/ai/spring_ai/config/CorsConfig.java
+++ spring-ai/src/main/java/com/ai/spring_ai/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://ai-image-generataion.netlify.app/")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
